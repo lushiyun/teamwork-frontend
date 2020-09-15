6f7bdc4306829a7cd09b08a61e2c8935c3568ec2
github username <lushiyun@outlook.com>
2020-09-15 18:26:45 -0400

    Complete leave team feature

--- src/features/teams/TeamsList.js
+++ src/features/teams/TeamsList.js
@@ -1,23 +1,36 @@
-import React, { useState, useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
 import {
   List,
   ListSubheader,
   ListItem,
   Avatar,
   ListItemText,
+  Menu,
+  MenuItem,
 } from '@material-ui/core'
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
 
-import { selectAllTeams, fetchTeams } from './teamsSlice'
+import React, { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+import {
+  selectAllTeams,
+  fetchTeams,
+  updateTeamMember,
+  selectTeamById,
+} from './teamsSlice'
 import LoadingBackdrop from '../../app/LoadingBackdrop'
+import { setSnackbar } from '../../ui/snackbarSlice'
+import { unwrapResult } from '@reduxjs/toolkit'
 
 const TeamsList = () => {
-  const [selectedIndex, setSelectedIndex] = useState(1)
-  const handleListItemClick = (e, index) => setSelectedIndex(index)
+  const [selectedId, setSelectedId] = useState(null)
+  const handleListItemClick = (e, id) => setSelectedId(id)
+  const [anchorEl, setAnchorEl] = useState(null)
+  const handleMoreIconClick = (e) => setAnchorEl(e.currentTarget)
 
   const dispatch = useDispatch()
   const teams = useSelector(selectAllTeams)
+  const selectedTeam = useSelector((state) => selectTeamById(state, selectedId))
   const teamsStatus = useSelector((state) => state.teams.status)
   const error = useSelector((state) => state.teams.error)
 
@@ -29,15 +42,54 @@ const TeamsList = () => {
 
   let content
 
+  const handleDetailsClick = () => {
+    return <div></div>
+  }
+
+  const serverError = {
+    open: true,
+    type: 'error',
+    message: 'Server busy, try again later',
+  }
+
+  const successMessage = (name) => ({
+    open: true,
+    type: 'success',
+    message: `Left ${name} successfully`,
+  })
+
+  const [leaveRequestStatus, setLeaveRequestStatus] = useState('idle')
+  const handleLeaveClick = async () => {
+    if (leaveRequestStatus !== 'idle') {
+      dispatch(setSnackbar(serverError))
+    } else {
+      try {
+        const updatedMembers = selectedTeam.userIds.filter((id) => id !== '45')
+        const resultAction = await dispatch(
+          updateTeamMember({
+            id: selectedId,
+            data: { user_ids: updatedMembers },
+          })
+        )
+        unwrapResult(resultAction)
+        dispatch(setSnackbar(successMessage(selectedTeam.name)))
+      } catch (err) {
+        console.error('Failed to save the team: ', err)
+      } finally {
+        setLeaveRequestStatus('idle')
+      }
+    }
+  }
+
   if (teamsStatus === 'loading') {
     content = <LoadingBackdrop />
   } else if (teamsStatus === 'succeeded') {
-    content = teams.map((team, index) => (
+    content = teams.map((team) => (
       <ListItem
         key={team.id}
         button
-        selected={selectedIndex === index}
-        onClick={(event) => handleListItemClick(event, index)}>
+        selected={selectedId === team.id}
+        onClick={(event) => handleListItemClick(event, team.id)}>
         <Avatar
           variant="rounded"
           src={team.cover_url}
@@ -45,7 +97,7 @@ const TeamsList = () => {
           style={{ marginRight: '1rem' }}
         />
         <ListItemText primary={team.name} />
-        <MoreHorizIcon />
+        <MoreHorizIcon onClick={handleMoreIconClick} />
       </ListItem>
     ))
   } else if (teamsStatus === 'error') {
@@ -62,6 +114,14 @@ const TeamsList = () => {
         </ListSubheader>
       }>
       {content}
+      <Menu
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={() => setAnchorEl(null)}>
+        <MenuItem onClick={handleDetailsClick}>Details</MenuItem>
+        <MenuItem onClick={handleLeaveClick}>Leave</MenuItem>
+      </Menu>
     </List>
   )
 }
